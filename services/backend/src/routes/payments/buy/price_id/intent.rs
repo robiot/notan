6855f0b7b102c6f641ab3a4a6e4bb3c2c3974fe0
Elliot <me@robiot.dev>
2023-12-18 21:07:13 +0200

    make intent and subscribe actually work

--- services/backend/src/routes/payments/buy/price_id/intent.rs
+++ services/backend/src/routes/payments/buy/price_id/intent.rs
@@ -1,9 +1,6 @@
 use crate::{
-    auth::check_auth,
-    error::Result,
-    routes::{payments::IntentResponse, Response},
-    state::AppState,
-    utils::payments::{customer, prices::get_price_value_from_price},
+    auth::check_auth, error::Result, routes::Response, schemas, state::AppState,
+    utils::payments::customer,
 };
 
 use {
@@ -11,25 +8,18 @@ use {
     axum::Json,
     hyper::HeaderMap,
     hyper::StatusCode,
-    serde::{Deserialize, Serialize},
     std::str::FromStr,
     std::sync::Arc,
 };
 
-use super::BuyIdParams;
-
-#[derive(Serialize, Deserialize)]
-pub struct ProductIntentBody {
-    // if card_token is not provided, you have to use elements
-    pub card_token: Option<String>,
-}
+use super::{BuyIdParams, PaymentMethodRequiredBody};
 
 pub async fn handler(
     State(state): State<Arc<AppState>>,
     Path(params): Path<BuyIdParams>,
     headers: HeaderMap,
-    Json(body): Json<ProductIntentBody>,
-) -> Result<Response<IntentResponse>> {
+    Json(body): Json<PaymentMethodRequiredBody>,
+) -> Result<Response<String>> {
     let id = check_auth(headers.clone(), state.clone()).await?;
     let customer = customer::get_stripe_customer_by_user_id(id.clone(), state.clone()).await?;
 
@@ -40,42 +30,27 @@ pub async fn handler(
     .fetch_one(&state.db)
     .await?;
 
-    let price = stripe::Price::retrieve(
-        &state.stripe,
-        &stripe::PriceId::from_str(&params.price_id.clone())?,
-        &["product"],
-    )
-    .await?;
+    let stripe_price_data =
+        sqlx::query_as::<sqlx::Postgres, schemas::stripe_price_currency::StripePriceCurrency>(
+            r#"SELECT * FROM public.stripe_price_currencies WHERE stripe_price_id = $1 AND currency = 'eur';"#,
+        )
+        .bind(params.price_id.clone())
+        .fetch_one(&state.db)
+        .await?;
 
     // create payment intent, we only use EUR for now
-    let mut payment_intent_data = stripe::CreatePaymentIntent::new(
-        get_price_value_from_price(price.clone())?,
-        stripe::Currency::EUR,
-    );
+    let mut payment_intent_data =
+        stripe::CreatePaymentIntent::new(stripe_price_data.price, stripe::Currency::EUR);
 
     payment_intent_data.payment_method_types = Some(vec!["card".to_string()]);
     payment_intent_data.customer = Some(customer.id);
     payment_intent_data.receipt_email = Some(&user.email);
+    payment_intent_data.confirm = Some(true);
+    payment_intent_data.payment_method = Some(stripe::PaymentMethodId::from_str(
+        body.payment_method_id.as_str(),
+    )?);
 
-    // payment_intent.confirm = Some(true);
+    stripe::PaymentIntent::create(&state.stripe, payment_intent_data).await?;
 
-    // if card_token is provided, use it
-    if let Some(card_token) = body.card_token {
-        payment_intent_data.payment_method =
-            Some(stripe::PaymentMethodId::from_str(card_token.as_str())?);
-    }
-    // else use elements and save the card
-    else {
-        payment_intent_data.setup_future_usage =
-            Some(stripe::PaymentIntentSetupFutureUsage::OffSession);
-    }
-
-    let payment_intent = stripe::PaymentIntent::create(&state.stripe, payment_intent_data).await?;
-
-    Ok(Response::new_success(
-        StatusCode::OK,
-        Some(IntentResponse {
-            client_secret: payment_intent.client_secret.unwrap(),
-        }),
-    ))
+    Ok(Response::new_success(StatusCode::OK, None))
 }
--- services/backend/src/routes/payments/buy/price_id/subscribe.rs
+++ services/backend/src/routes/payments/buy/price_id/subscribe.rs
@@ -1,9 +1,5 @@
 use crate::{
-    auth::check_auth,
-    error::{Error, Result},
-    routes::{payments::IntentResponse, Response},
-    state::AppState,
-    utils::payments::customer,
+    auth::check_auth, error::Result, routes::Response, state::AppState, utils::payments::customer,
 };
 
 use {
@@ -11,70 +7,39 @@ use {
     axum::Json,
     hyper::HeaderMap,
     hyper::StatusCode,
-    serde::{Deserialize, Serialize},
-    std::str::FromStr,
     std::sync::Arc,
 };
 
-use super::BuyIdParams;
-
-#[derive(Serialize, Deserialize)]
-pub struct SubscriptionSubscribeBody {
-    // card has to be created before
-    pub card_token: String,
-}
+use super::{BuyIdParams, PaymentMethodRequiredBody};
 
 pub async fn handler(
     State(state): State<Arc<AppState>>,
     Path(params): Path<BuyIdParams>,
     headers: HeaderMap,
-    Json(body): Json<SubscriptionSubscribeBody>,
-) -> Result<Response<IntentResponse>> {
+    Json(body): Json<PaymentMethodRequiredBody>,
+) -> Result<Response<String>> {
     let id = check_auth(headers.clone(), state.clone()).await?;
     let customer = customer::get_stripe_customer_by_user_id(id.clone(), state.clone()).await?;
 
-    let price = stripe::Price::retrieve(
-        &state.stripe,
-        &stripe::PriceId::from_str(&params.price_id.clone())?,
-        &[],
-    )
-    .await?;
+    // todo: cancel previous subscription
 
     // Subscription
     let mut subscription_data = stripe::CreateSubscription::new(customer.id);
     subscription_data.currency = Some(stripe::Currency::EUR);
-    subscription_data.payment_behavior =
-        Some(stripe::SubscriptionPaymentBehavior::DefaultIncomplete);
-    subscription_data.expand = &["latest_invoice.payment_intent"];
-
     subscription_data.items = Some(vec![stripe::CreateSubscriptionItems {
-        price: Some(price.id.to_string()),
+        price: Some(params.price_id.clone()),
+
         ..Default::default()
     }]);
+    subscription_data.automatic_tax =
+        Some(stripe::CreateSubscriptionAutomaticTax { enabled: true });
+    subscription_data.expand = &["latest_invoice.payment_intent"];
+    subscription_data.collection_method = Some(stripe::CollectionMethod::ChargeAutomatically);
 
-    let card_token_str = body.card_token.clone();
-    subscription_data.default_payment_method = Some(&card_token_str);
+    let payment_method_id_str = body.payment_method_id.clone();
+    subscription_data.default_payment_method = Some(&payment_method_id_str);
 
-    let subscription = stripe::Subscription::create(&state.stripe, subscription_data).await?;
+    stripe::Subscription::create(&state.stripe, subscription_data).await?;
 
-    let client_secret = subscription
-        .latest_invoice
-        .clone()
-        .and_then(|invoice| match invoice {
-            stripe::Expandable::Object(invoice) => invoice.payment_intent,
-            _ => None,
-        })
-        .and_then(|payment_intent| match payment_intent {
-            stripe::Expandable::Object(payment_intent) => payment_intent.client_secret,
-            _ => None,
-        })
-        .ok_or(Error::StripeFieldNotFoundError(
-            "Price Subscription invoice.payment_intent, or payment_intent.client_secret"
-                .to_string(),
-        ))?;
-
-    Ok(Response::new_success(
-        StatusCode::OK,
-        Some(IntentResponse { client_secret }),
-    ))
+    Ok(Response::new_success(StatusCode::OK, None))
 }
