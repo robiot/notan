08775d2837f6421f9c425cb687c67b82cbb270a9
Elliot <me@robiot.dev>
2023-12-20 17:15:49 +0200

    init app.notan.ax

---
+++ ui/app/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import "@/styles/index.css";
+
+import { Noto_Sans } from "next/font/google";
+import { ReactNode } from "react";
+
+import { cn } from "@/lib/utils";
+
+const noto_sans = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(noto_sans.className, "bg-background text-foreground")}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ ui/app/src/app/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Home() {
+  return <>这是太酷了，明天</>;
+}
