56fec10dbaa9c8f1c60681a7f91107a95cf8f3da
Elliot <me@robiot.dev>
2023-12-23 13:27:35 +0200

    fix login redirect

--- ui/app/src/app/(auth)/layout.tsx
+++ ui/app/src/app/(auth)/layout.tsx
@@ -1,8 +1,21 @@
 "use client";
 
-import { FC, ReactNode } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { FC, ReactNode, useEffect } from "react";
+
+import { useAuth } from "@/hooks/useAuth";
 
 const AuthLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  const router = useRouter();
+  const auth = useAuth();
+  const path = usePathname();
+
+  useEffect(() => {
+    if (auth.token) {
+      router.push("/");
+    }
+  }, [auth, router, path]);
+
   return <>{children}</>;
 };
 
