971dcfe55742e975dd8a4a8a61e3c76c4e89b480
Elliot <me@robiot.dev>
2023-12-23 12:45:55 +0200

    drag to open

--- ui/extension/src/core/popup/pages/home/_components/NotePreview.tsx
+++ ui/extension/src/core/popup/pages/home/_components/NotePreview.tsx
@@ -1,19 +1,27 @@
 import { Button } from "@popup/components/ui/button";
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { Note } from "@/core/popup/hooks/notes/useNotes";
 import { faviconFromUrl } from "@/core/popup/lib/favicon";
 import { cn } from "@/core/popup/lib/utils";
 
 export const NotePreview: FC<{ note: Note }> = ({ note }) => {
+  const navigate = useNavigate();
+
   return (
     <Button
       variant="ghost"
       title={note.url}
       className={cn("flex justify-start items-start font-normal gap-3 text-start p-2 bg-card h-fit")}
       asChild>
-      <Link to={`/notes/view/${note.id}`}>
+      <Link
+        to={note.url}
+        onClick={(event) => {
+          event.preventDefault();
+
+          navigate(`/notes/view/${note.id}`);
+        }}>
         <div>
           <img src={faviconFromUrl(note.url)} alt="" className="min-w-[1.5rem] w-6 h-6 rounded" />
         </div>
