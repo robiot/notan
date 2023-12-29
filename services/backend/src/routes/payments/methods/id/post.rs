cce03a8ca675a0d1f1558b3996bd4d1c9c88f7ac
Elliot <me@robiot.dev>
2023-12-29 16:49:06 +0200

    Payment method attach error handling

--- services/backend/src/routes/payments/methods/id/post.rs
+++ services/backend/src/routes/payments/methods/id/post.rs
@@ -1,5 +1,5 @@
 use crate::{
-    auth::check_auth, error::Result, routes::Response, state::AppState, utils::payments::customer,
+    auth::check_auth, error::{Result, Error}, routes::{Response, ResponseError}, state::AppState, utils::payments::customer,
 };
 
 use super::PaymentMethodIdParams;
@@ -20,14 +20,49 @@ pub async fn handler(
     let id = check_auth(headers.clone(), state.clone()).await?;
     let customer = customer::get_stripe_customer_by_user_id(id.clone(), state.clone()).await?;
 
-    stripe::PaymentMethod::attach(
+    match stripe::PaymentMethod::attach(
         &state.stripe,
         &stripe::PaymentMethodId::from_str(&params.payment_method_id)?,
         stripe::AttachPaymentMethod {
             customer: customer.id,
         },
     )
-    .await?;
+    .await {
+        Ok(a) => Ok(a),
+        Err(err) => {
+            match err {
+                stripe::StripeError::Stripe(request_error) => {
+                    match request_error.error_type {
+                        stripe::ErrorType::Card => {
+                            return Err(Error::BadRequest(ResponseError {
+                                message: request_error
+                                    .message
+                                    .unwrap_or("unknown_error".to_string()),
+                                name: "card_error".to_string(),
+                            }));
+                        }
+                        _ => {
+                            // Handle other types of Stripe errors
+                            log::error!("Stripe error on payment method attach: {:?}", request_error);
+
+                            return Err(Error::BadRequest(ResponseError {
+                                message: request_error
+                                    .message
+                                    .unwrap_or("unknown_error".to_string()),
+                                name: "method_error".to_string(),
+                            }));
+                        }
+                    }
+                }
+                _ => {
+                    // Handle other types of errors
+                    println!("Other methods error: {:?}", err);
+
+                    Err(err)
+                }
+            }
+        }
+    }?;
 
     Ok(Response::new_success(StatusCode::OK, None))
 }
