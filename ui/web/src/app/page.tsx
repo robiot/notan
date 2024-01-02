aeb7628e50c940f46a0cd65de278dc6fe784538f
Elliot <me@robiot.dev>
2024-01-02 23:47:20 +0200

    random

--- ui/web/src/app/page.tsx
+++ ui/web/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { HomeAnywhereSection } from "@/components/fragments/Home/Anywhere/Anywhere";
 import { HomeBrowsersSection } from "@/components/fragments/Home/Browsers/Browsers";
-import { HomeJoinSection } from "@/components/fragments/Home/Join/Join";
+// import { HomeJoinSection } from "@/components/fragments/Home/Join/Join";
 import { NotesLikeBookmarksSection } from "@/components/fragments/Home/NotesLikeBookmarks/NotesLikeBookmarks";
 import { HomeSearchSection } from "@/components/fragments/Home/Search/Search";
 import { HomeTopSection } from "@/components/fragments/Home/Top/Top";
